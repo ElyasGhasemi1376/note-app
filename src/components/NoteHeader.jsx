ee07d7687a6ee034271b5843065db98ac193bbdc
elyas.ghasemi <elyas.ghasemi@gmail.com>
2025-01-26 11:49:56 +0330

    select option size in NoteHeader.jsx has been modified.

--- src/components/NoteHeader.jsx
+++ src/components/NoteHeader.jsx
@@ -1,22 +1,22 @@
 export default function NoteHeader({ notes, setSortBy }) {
   return (
     <div>
-      <div className="w-full flex justify-between items-center">
+      <div className="w-full flex justify-between items-center gap-6">
         <div>
-          <h1 className="flex items-center justify-center gap-2">Note header  <span className="bg-blue-500 text-xs text-white size-3 flex justify-center items-center p-2.5 rounded-full">
-            {notes.length}
-          </span></h1>
+          <h1 className="whitespace-nowrap flex items-center justify-center gap-2 md:gap-0">
+            Notes
+          </h1>
         </div>
         <div>
           <select
             onChange={(e) => setSortBy(e.target.value)}
             name={notes.id}
             id={notes.id}
-            className="rounded-lg"
+            className="rounded-lg text-sm  px-2 py-1 md:text-base md:px-3 md:py-2"
           >
-            <option value="latest">sort based on latest notes</option>
-            <option value="earliest">sort based on earliest notes</option>
-            <option value="completed">sort based on completed notes</option>
+            <option value="latest"> latest notes</option>
+            <option value="earliest"> earliest notes</option>
+            <option value="completed"> completed notes</option>
           </select>
         </div>
       </div>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/note-app/',
   plugins: [react()],
-})
+  server: {
+    port: 5173,
+    open: true,
+  },
+});
